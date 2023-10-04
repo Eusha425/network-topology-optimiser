308f22dc1271e9047fd41a060adb0230a16aec54
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2023-10-04 22:01:30 +1100

    added code to the main which uses data to try to solve the problem

--- main.c
+++ main.c
@@ -1,23 +1,50 @@
 #include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+// Function to assign random weights to edges
+int assignRandomWeight() {
+    return rand() % 10 + 1; // Generates random weights between 1 and 10
+}
 
 int main() {
-    int V = 4; // Number of vertices
-    Graph* graph = createGraph(V);
+    int rows = 5; // Number of rows in the grid
+    int cols = 5; // Number of columns in the grid
+    Graph* graph = createGraph(rows * cols);
 
-    addEdge(graph, 0, 1, 10);
-    addEdge(graph, 0, 2, 6);
-    addEdge(graph, 0, 3, 5);
-    addEdge(graph, 1, 3, 15);
-    addEdge(graph, 2, 3, 4);
-    addEdge(graph, 0, 1, 2);
-    addEdge(graph, 0, 2, 3);
-    addEdge(graph, 0, 3, 1);
-    addEdge(graph, 1, 2, 4);
-    addEdge(graph, 1, 4, 5);
-    addEdge(graph, 2, 3, 6);
-    addEdge(graph, 3, 4, 7);
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Create a grid with coverage and interference radii (simplified)
+    int grid[5][5][2] = {
+        {{1, 2}, {1, 2}, {1, 2}, {0, 0}, {0, 0}},
+        {{1, 2}, {2, 3}, {2, 3}, {2, 3}, {0, 0}},
+        {{1, 2}, {2, 3}, {2, 3}, {2, 3}, {0, 0}},
+        {{0, 0}, {2, 3}, {2, 3}, {2, 3}, {0, 0}},
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
+    };
+
+    // Add edges between tower locations with random weights
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            int src = i * cols + j;
+            int interferenceThreshold = grid[i][j][1]; // Maximum allowed interference
+            for (int x = i - grid[i][j][0]; x <= i + grid[i][j][0]; ++x) {
+                for (int y = j - grid[i][j][0]; y <= j + grid[i][j][0]; ++y) {
+                    if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                        int dest = x * cols + y;
+                        if (src != dest && interferenceThreshold > 0) {
+                            // Assign random weight to the edge
+                            int weight = assignRandomWeight();
+                            addEdge(graph, src, dest, weight);
+                            interferenceThreshold--;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     kruskalMST(graph);
 
