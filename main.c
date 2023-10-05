5fc1eb3516058f72b288fa107da382c0bf591ee7
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2023-10-05 21:38:46 +1100

    test code for create graph and adding the edge.

--- main.c
+++ main.c
@@ -71,7 +71,7 @@ int main_edge() {
 }
 
 // testing find and unionsets 
-int main() {
+int main_union() {
     Subset subsets[5];
 
     for (int i = 0; i < 5; i++) {
@@ -92,6 +92,33 @@ int main() {
     printf("Root of 0: %d\n", root0); // Should print the root (representative) of the set containing 0 and 1
     printf("Root of 2: %d\n", root2); // Should print the root (representative) of the set containing 2, 3, and 1
     printf("Root of 4: %d\n", root4); // Should print 4 since it's not part of any union
+
+    return 0;
+}
+
+
+// test for create graph and adding edge
+int main() {
+    int V = 4; // Number of vertices
+    Graph* graph = createGraph(V);
+
+    // Add edges to the graph
+    addEdge(graph, 0, 1, 2, 3);
+    addEdge(graph, 0, 2, 4, 1);
+    addEdge(graph, 1, 2, 5, 2);
+
+    // Print the adjacency list to verify edge addition
+    for (int i = 0; i < V; ++i) {
+        printf("Adjacency List of Vertex %d:\n", i);
+        Node* current = graph->array[i].head;
+        while (current) {
+            printf("-> %d (Interference: %d, Cost: %d)\n", current->dest, current->weight, current->cost);
+            current = current->next;
+        }
+    }
+
+    // Free allocated memory
+    free(graph);
 
     return 0;
 }
